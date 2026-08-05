64ecfe199f4548d08723fd0263a082e3ec03f254
tiago <tiagohirute@gmail.com>
2026-08-05 17:56:25 -0300

    new scene test

--- Moonlit/Assets/Scripts/Utility.cs
+++ Moonlit/Assets/Scripts/Utility.cs
@@ -1,7 +1,18 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Utility : MonoBehaviour
 {
+
+    [SerializeField] private string sceneName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
 
     public void ToggleObj(GameObject G_object)
     {
